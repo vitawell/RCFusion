0b1654dc27809689e3ab290771e87e0fa785da67
vitawell <41141374+vitawell@users.noreply.github.com>
2022-03-16 16:32:01 +0800

    Update train_and_eval.py

--- code/train_and_eval.py
+++ code/train_and_eval.py
@@ -24,8 +24,8 @@ from keras.metrics import categorical_accuracy
 from keras import backend as K
 
 from tensorflow.python.client import device_lib
-print device_lib.list_local_devices()
-
+# print device_lib.list_local_devices()
+print(device_lib.list_local_devices())
 
 
 
